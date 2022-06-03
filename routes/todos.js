d0ad5ad4474df59d9191a32a8cb3907c9055a5a4
Denny <calgary.dany@gmail.com>
2022-06-03 14:31:31 +0300

    drag and drop patch

--- routes/todos.js
+++ routes/todos.js
@@ -8,24 +8,9 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 router.get('/todo', async (req, res) => {
-    const todos = await TodoList.find({});
-    console.log(`todos was getted`);
-    res.send(todos);
-})
-
-router.put('/todo', async (req, res) => {
-    // неактуальный запрос на смену статуса, заменить на пут.драгендропп
-    // const todo = new Todo({
-    //     id: req.body.id,
-    //     title: req.body.title,
-    //     status: req.body.status,
-    // })
-    // try {
-    //     await Todo.findOneAndReplace({id:todo.id, title: todo.title, status: !todo.status},{id:todo.id, title: todo.title, status: todo.status})
-    //     console.log(`${todo} was changed`);
-    // } catch (e) {
-    //     console.log(`Error: ${e.message}`);
-    // }
+    const todoList = await TodoList.find({});
+    console.log(`todoList was getted`);
+    res.send(todoList);
 })
 
 router.delete('/todo-list', async (req, res) => {
@@ -35,7 +20,7 @@ router.delete('/todo-list', async (req, res) => {
         todos: req.body.todos,
     });
     try {
-        await TodoList.remove({ name: todoList.name, collectionId: todoList.collectionId, todos: todoList.todos})
+        await TodoList.deleteOne({ name: todoList.name, collectionId: todoList.collectionId, todos: todoList.todos})
         console.log(`${todoList} was deleted`);
     } catch (e) {
         console.log(`Error: ${e.message}`);
@@ -62,23 +47,67 @@ router.post('/todo', async (req, res) => {
         id: req.body.id,
         title: req.body.title,
         status: req.body.status,
-      })
-      todoList.todos.push(todo);
+    })
+    todoList.todos.push(todo);
     try {
         await todoList.save();
-        console.log(`${todoList} was posted`);
+        console.log(`${todo} was posted`);
+    } catch (e) {
+        console.log(`Error: ${e.message}`);
+    }
+})
+
+router.post('/change', async (req, res) => {
+    let todoList = await TodoList.find({});
+    let todo = new Todo({
+        id: req.body.todo.id,
+        title: req.body.todo.title,
+        status: req.body.todo.status,
+    });
+    let newCollId = req.body.newListCollectionId;
+    let newTaskIndex = req.body.newTaskIndex;
+    console.log(req.body)
+
+    let idOfTodoList;
+    let idOfTodo;
+    for (let i=0; i<todoList.length; i++) {
+        for (let j=0; j<todoList[i].todos.length; j++) {
+            if (todoList[i].todos[j].id === todo.id && todoList[i].todos[j].title === todo.title && todoList[i].todos[j].status === todo.status ) {
+                idOfTodoList = todoList[i].collectionId
+                idOfTodo = todoList[i].todos[j].id
+                todoList[i].todos.splice(j, 1);
+                break;
+            }
+        }
+    } // delete old
+
+    let indexOfTodoList;
+    for (let i=0; i<todoList.length; i++) {
+        if (todoList[i].collectionId === newCollId) {
+            todoList[i].todos.splice(newTaskIndex, 0, todo);
+            indexOfTodoList = i
+        }
+    } // add new
+
+    try {
+        await TodoList.updateOne({collectionId: idOfTodoList}, {$pull : { todos : { id : idOfTodo} } } ); // delete old
+        await todoList[indexOfTodoList].save(); // add new
+        console.log(`DRAG AND DROP WAS UPDATED`);
     } catch (e) {
         console.log(`Error: ${e.message}`);
     }
 })
 
 router.delete('/todo', async (req, res) => {
-    const todoList = await TodoList.find({});
-    const todo = new Todo({
+    let todoList = await TodoList.find({});
+    let todo = new Todo({
         id: req.body.id,
         title: req.body.title,
         status: req.body.status,
     });
+    if(!todo || !todoList){
+        return res.status(404).json({message:'not found'})
+    }   
     let idOfTodoList;
     let idOfTodo;
     for (let i=0; i<todoList.length; i++) {
@@ -91,7 +120,7 @@ router.delete('/todo', async (req, res) => {
         }
     }
     try {
-        await TodoList.updateOne({collectionId: idOfTodoList}, {$pull : { todos : {id : idOfTodo} } } );
+        await TodoList.updateOne({collectionId: idOfTodoList}, {$pull : { todos : { id : idOfTodo} } } );
         console.log(`WAS DELETED`);
     } catch (e) {
         console.log(`Error: ${e.message}`);
@@ -99,3 +128,10 @@ router.delete('/todo', async (req, res) => {
 })
 
 module.exports = router
+
+
+
+
+
+// var removed = myFish.splice(3, 1); удаляем что-то по индексу 3
+//var removed = myFish.splice(2, 0, 'drum'); вставляем текст по индексу 2 и не удаляем ничего
